7ee68c27a27dfddac21577a1a473461beea247d9
tontonsat <hubert.pfersdorff@gmail.com>
2019-02-01 15:44:11 +0100

    ajout comment & category + faker data

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -5,6 +5,8 @@ namespace App\DataFixtures;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use App\Entity\Article;
+use App\Entity\Category;
+use App\Entity\Comment;
 use Faker;
 
 class ArticleFixtures extends Fixture
@@ -14,13 +16,34 @@ class ArticleFixtures extends Fixture
         // On configure dans quelles langues nous voulons nos données
         $faker = Faker\Factory::create('fr_FR');
 
-        for($i=1; $i<= 10; $i++) {
-            $article = new Article();
-            $article->setTitle($faker->name)
-                    ->setContent($faker->text($maxNbChars = 200))
-                    ->setCreatedAt($faker->dateTime($max = 'now', $timezone = null))
-                    ->setImage("http://placehold.it/350x150");
-            $manager->persist($article);
+        for($i=1; $i < 3; $i++) {
+            $category = new Category();
+            $category->setTitle($faker->sentence())
+                    ->setDescription($faker->paragraph());
+            $manager->persist($category);
+
+            for($j=1; $j<= 10; $j++) {
+                $article = new Article();
+                $article->setTitle($faker->name)
+                        ->setContent($faker->text($maxNbChars = 200))
+                        ->setCategory($category)
+                        ->setCreatedAt($faker->dateTime($max = 'now', $timezone = null))
+                        ->setImage("http://placehold.it/350x150");
+                $manager->persist($article);
+            }
+            $now = new \Datetime();
+            $interval = $now->diff($article->getCreatedAt());
+            $days = $interval->days;
+            $minimum = "-". $days ." days";
+
+            for ($k=0; $k < mt_rand(4, 10); $k++) {
+                $comment = new Comment();
+                $comment->setContent($faker->text($maxNbChars = 80))
+                        ->setCreatedAt($faker->dateTime($minimum))
+                        ->setArticle($article);
+                $manager->persist($comment);
+
+            }
         }
         $manager->flush();
     }
