a9ac01af81d71e063e43844fbf17ac9089a63755
tontonsat <hubert.pfersdorff@gmail.com>
2019-02-05 09:21:29 +0100

    working display

--- src/DataFixtures/ArticleFixtures.php
+++ src/DataFixtures/ArticleFixtures.php
@@ -16,7 +16,7 @@ class ArticleFixtures extends Fixture
         // On configure dans quelles langues nous voulons nos données
         $faker = Faker\Factory::create('fr_FR');
 
-        for($i=1; $i < 3; $i++) {
+        for($i=0; $i < 10; $i++) {
             $category = new Category();
             $category->setTitle($faker->sentence())
                     ->setDescription($faker->paragraph());
@@ -28,21 +28,23 @@ class ArticleFixtures extends Fixture
                         ->setContent($faker->text($maxNbChars = 200))
                         ->setCategory($category)
                         ->setCreatedAt($faker->dateTime($max = 'now', $timezone = null))
-                        ->setImage("http://placehold.it/350x150");
+                        ->setImage($faker->imageUrl());
+                        
                 $manager->persist($article);
-            }
-            $now = new \Datetime();
-            $interval = $now->diff($article->getCreatedAt());
-            $days = $interval->days;
-            $minimum = "-". $days ." days";
 
-            for ($k=0; $k < mt_rand(4, 10); $k++) {
-                $comment = new Comment();
-                $comment->setContent($faker->text($maxNbChars = 80))
-                        ->setCreatedAt($faker->dateTime($minimum))
-                        ->setArticle($article);
-                $manager->persist($comment);
+                $now = new \Datetime();
+                $interval = $now->diff($article->getCreatedAt());
+                $days = $interval->days;
+                $minimum = "-". $days ." days";
 
+                for ($k=0; $k < mt_rand(4, 10); $k++) {
+                    $comment = new Comment();
+                    $comment->setContent($faker->text($maxNbChars = 80))
+                            ->setCreatedAt($faker->dateTime($minimum))
+                            ->setArticle($article);
+                    $manager->persist($comment);
+
+                }
             }
         }
         $manager->flush();
